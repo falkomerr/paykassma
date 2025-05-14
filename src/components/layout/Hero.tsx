85d024e8f627eb6ec9e43d42992a26e27c603733
Roman Paravaev <105551615+falkomerr@users.noreply.github.com>
2025-05-15 00:03:33 +0300

    feat(layout): integrate Spline animation into Hero component
    Replace the LogoBig component with a Spline animation for enhanced visual appeal in the Hero section. Introduce a new Spline instance to display a 3D scene, improving user engagement. Adjust layout styles for better positioning of the text and animation.

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,8 +1,8 @@
 import { gateOpened } from '@/models/journey';
+import Spline from '@splinetool/react-spline';
 import { useUnit } from 'effector-react';
 import { useTranslation } from '../../hooks/useTranslation';
 import { AnimatedButton } from '../ui/AnimatedButton';
-import { LogoBig } from '../ui/LogoBig';
 
 export const Hero = () => {
   const openGate = useUnit(gateOpened);
@@ -13,9 +13,14 @@ export const Hero = () => {
       id="home"
       className="relative z-10 flex min-h-[calc(100vh-120px)] flex-col items-center justify-center">
       <div className="mb-4">
-        <LogoBig />
+        <div className="h-[30rem] w-screen overflow-hidden bg-black">
+          <Spline
+            scene="https://prod.spline.design/VesmLhzpdoKoAkRX/scene.splinecode"
+            className="scale-[2]"
+          />
+        </div>
       </div>
-      <div className="gilroy mb-10 max-w-[480px] text-center text-[22px] leading-none font-normal text-[#ccc]">
+      <div className="gilroy -mt-[10rem] mb-10 max-w-[480px] text-center text-[22px] leading-none font-normal text-[#ccc]">
         {t('hero.description')}
       </div>
       <AnimatedButton variant="login" size="big" onClick={openGate}>
